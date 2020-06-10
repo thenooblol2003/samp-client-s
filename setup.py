de6a57f12ed41a8c78f18c6f9c914c32ce9fff9f
thenooblol2003 <thenooblol2003@gmail.com>
2020-06-10 19:18:42 -0400

    Query with spanish character support for Discord Bot

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from distutils.core import setup
+try:
+    long_description = open('README.md', 'r').read()
+except:
+    long_description = ''
+
+setup(
+    name='samp_client-ES',
+    version='1.0.0',
+    packages=['samp_client-ES'],
+    url='https://github.com/thenooblol2003/samp-client-s',
+    license='None',
+    author='Michal Dabski',
+    author_email='contact@michaldabski.com',
+    install_requires=['future'],
+    description='SA-MP API client for python supporting both query and RCON APIs, plus, spanish decoding support',
+    long_description=long_description,
+    download_url='https://github.com/thenooblol2003/samp_client-ES/archive/1.0.0.tar.gz',
+)
